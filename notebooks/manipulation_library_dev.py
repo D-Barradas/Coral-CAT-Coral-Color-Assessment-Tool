a367410645fd7568560cc67cda9d0689f7b80de4
Didier Barradas Bautista <didier.barradas@gmail.com>
2025-03-13 15:37:28 +0300

    added more funtion to make all in one library

--- notebooks/manipulation_library_dev.py
+++ notebooks/manipulation_library_dev.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-import os 
+import os , sys
 from glob import glob 
 from collections import Counter, defaultdict
 from skimage.color import rgb2lab, deltaE_cie76
 import pandas as pd
 from sklearn.cluster import KMeans
+import torch
 
 
 def get_image(image_path):
@@ -416,3 +417,163 @@ class OcrAnalysis:
         plt.tight_layout()
         plt.show()
 
+def is_cuda_available():
+    """Checks if CUDA is available and can be used by PyTorch.
+
+    Returns:
+        bool: True if CUDA is available, False otherwise.
+    """
+
+    return torch.cuda.is_available()
+
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2)) 
+
+
+
+## Define helper functions for visualization
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+
+
+## Preprocess the image
+def preprocess_histograms(image):
+    #seperating colour channels
+    B = image[:,:,0] #blue layer
+    G = image[:,:,1] #green layer
+    R = image[:,:,2] #red layer
+    # equalize the histograms 
+    b_equi = cv2.equalizeHist(B)
+    g_equi = cv2.equalizeHist(G)
+    r_equi = cv2.equalizeHist(R)
+    equi_im = cv2.merge([b_equi,g_equi,r_equi])
+    return equi_im
+
+def get_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = preprocess_histograms( image=image)
+    return image
+
+
+## Visualize images
+def show_images_grid(images, titles=None, figsize=(20, 20)):
+    """Displays a grid of images with optional titles."""
+
+    num_images = len(images)
+    rows = int(num_images / 2)
+    cols = 2
+
+    # Create a figure and subplots
+    fig, axes = plt.subplots(rows, cols, figsize=figsize)
+
+    # Flatten the subplots array for easier iteration
+    axes = axes.flatten()
+
+    for i, ax in enumerate(axes):
+        if i < num_images:
+            img = images[i]
+            ax.imshow(img)
+            # ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  # Convert to RGB for Matplotlib
+            ax.axis('off')  # Hide axes
+
+            if titles:
+                ax.set_title(titles[i])
+        else:
+            ax.axis('off')  # Hide unused subplots
+
+    plt.tight_layout()
+    plt.show()
+
+
+def load_sam_model():
+    sys.path.append('../')
+    from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
+
+    sam_checkpoint = "../checkpoints/vit_b_coralscop.pth"  # this is coralSCOP
+    model_type = "vit_b"
+
+    if is_cuda_available():
+        print("CUDA is available!")
+        device = torch.device("cuda")
+    else:
+        print("CUDA is not available. Using CPU.")
+        device = torch.device("cpu")
+
+
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+
+    # Parameters for CoralScop
+    points_per_side = 32
+    pred_iou_thresh = 0.72
+    stability_score_thresh = 0.62
+
+    mask_generator = SamAutomaticMaskGenerator(
+        model=sam,
+        points_per_side=points_per_side,
+        pred_iou_thresh=pred_iou_thresh,
+        stability_score_thresh=stability_score_thresh,
+        crop_n_layers=1,
+        crop_n_points_downscale_factor=2,
+        min_mask_region_area=100,  # Requires open-cv to run post-processing
+    )
+
+    return mask_generator
+
+
+
+def get_sorted_by_area(image, anns):
+    area_list=[]
+    cropped_image_dic ={}
+    mask_number = [] 
+    mask_pixles_dic = {}
+    for i in range(len(anns)):
+        x, y, width, height = anns[i]['bbox']
+        area = anns[i]["area"]
+        image_b, masked_pixels = background_to_black(image=image, index=anns[i])
+        cropped_image = image_b[int(y):int(y+height), int(x):int(x+width)]
+
+        area_list.append(area)
+        cropped_image_dic[i] = cropped_image
+        mask_pixles_dic[i] = masked_pixels
+        mask_number.append(i)
+    df = pd.DataFrame([area_list,mask_number])
+    df = df.T
+    df.columns = ['area','mask_number']
+    df.sort_values(by='area', ascending=False, inplace=True)
+    df.dropna(inplace=True)
+    return df , cropped_image_dic , mask_pixles_dic
+
+def process_images(image, masks):
+    """
+    Processes the given image and masks to extract and sort cropped images by area.
+
+    Args:
+        image (np.ndarray): The input image.
+        masks (list): List of mask annotations.
+
+    Returns:
+        list: List of cropped images.
+        list: List of titles for the images.
+    """
+    image_dataframe, cropped_image_list, mask_pixels_dict = get_sorted_by_area(image=image, anns=masks)
+    all_images_by_area = image_dataframe['mask_number'].to_list()
+    list_of_images = [cropped_image_list[idx] for idx in all_images_by_area]
+    titles = [f'Image {i+1}' for i in range(len(all_images_by_area))]
+    return list_of_images, titles
