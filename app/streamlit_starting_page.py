35cb1f2167c4918e06a8046ab0a8a50dd206bf3e
Didier Barradas Bautista <didier.barradas@gmail.com>
2025-03-13 10:31:16 +0300

    New markdown text added

--- app/streamlit_starting_page.py
+++ app/streamlit_starting_page.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to The app! 👋")
+st.write("# Welcome to the Coral CAT: Coral Color Assessment Tool! 👋")
 
 st.sidebar.success("Select each stage above, one by one.")
 
@@ -35,81 +35,57 @@ st.sidebar.success("Select each stage above, one by one.")
 
 st.markdown(
     """
-    
-# Interactive coral color analysis
+# Welcome to the Coral CAT: Coral Color Assessment Tool
 
-This project implements the segment anything algorithm, and asses the coral heatlh accoring to the color chart given.
-The project not only requires segmentation, it also applies Optical Character Recognition  (OCR) , it manipulates the image to create a Custom Color Chart 
-The combination of these techniques results in the mapping of colors over the coral of interest. 
-You are in the welcome page so lets think step by step 
+**Interactive coral color analysis**
 
+Coral CAT is a semi-automatic image analysis tool that assigns color codes to coral images. The pipeline allows the standardized and automatic extraction of the color (RGB) of each pixel of a cropped coral image. It assigns the closest color code extracted from the picture's color chart. We combine the available tools, such as CoralSCOP and Optical Character Recognition (OCR).
 
-## Usage/Examples
+## Usage
 
-Welcome to The app! 👋
-This is a modular application , meaning each of the names you see on the side bar is a stage on the sequence of steps to get the result . There are 3 main stages:
+This is a modular application, meaning that each tab on the sidebar is a consecutive step. The workflow is divided into three main steps:
 
+### Step 1 - Upload the image and define the color chart and coral fragment/colony areas
 
-### Step 1 - Cropping images 
-In this part you will select two section of your images:
-* the coral image
-* the color chart 
+In this tab, you can upload your image and define the areas corresponding to the color chart and the coral fragment of the colony. The specified and selected areas must be saved in the memory before moving to the next step.
 
-After this you will save the images into the memory of the machine 
+### Step 2 - Build a Custom Color Chart
 
-### Step 2 - Separate Colors from the chart
-In this section you are required to select the color sections from the color chart (Up, Down, Left, Right)
-Also, this assumes that you have a color chart as in the "Screenshot section".
+In this section, you are required to select the color hue sections from the color chart (Up, Down, Left, and Right corresponding to B, D, E, and C, respectively). It is important to keep the color hues in the right position.
 
-The sequence is as follows :
-* Select up, down ... , Try to get the letter as best a posible
-* Process the color crops - this will try to correct the titl on the image to make the OCR easier
-* Start the OCR by pressing "Build the Color Chart" button 
-    - if the program can not detect 6 labels it will tell you 
-    - you can reselect the area you have problems and process it again
-* If the OCR is succesful it will deploy the custom color chart and you can save it as a PNG file and also as text file.
+The sequence is as follows:
+- Select and save the four color hue areas; try to get the letter as best as possible
+- Process the color crops - this will try to correct the tilt on the image to make the OCR easier
+- Start the OCR by clicking on the "Build the Color Chart" button
+  - If the program cannot detect six labels, it will tell you
+  - You can reselect the area where you have problems and process it again
+- If the OCR is successful, it will show the custom color chart image, which can be saved as a PNG file and exported as a text file.
 
-### Step 3 - Mapping the custom color chart
-In this secction the we process the coral image and finaly analize the coral Colors
+### Step 3 - Color analysis and mapping
 
-Start with the segmentation of the image of the coral using a finetunes the SAM model (Thanks Meta AI ) called "CoralSCOP" (Thanks to the authors of the paper)
-* Press start segmentation button
-* Select the Image of the coral that you want to analyze
-* Trigger the analisis byt pressing "Analyze colors in the selected image" button 
-* Wait for the results
+In this section, the saved coral image area is segmented using a finetuned [SAM model](https://github.com/facebookresearch/segment-anything) called ["CoralSCOP"](https://github.com/zhengziqiang/CoralSCOP).  Please cite these amazing tools if you use them. 
+- Press the start segmentation button.
+- Select the image segment corresponding to the cropped coral area you want to analyze.
+- Start the analysis by clicking the "Analyze colors in the selected image" button.
 
-#### Interpreting the Results 
- The firts result you will get is a quick analysis of the color we can find on the coral as a pie chart that will tell you the percentaje of colors on the image
-- Then it wll show the color closer to a "Ideal color chart" - meaning it calculates the color closest to the color chart what we hardcoded with the correspoing RGB values from the color chart.
-- Finally the las images will be an figure that shows the original image, a mapped image generated with the colors from the custom color chart and the percentage of each custon color on the picture 
+The results provided include a color analysis.
 
-## Image manipulation sections 
+Finally, the last image will be a figure showing the original image, a mapped image generated with the colors from the custom color chart, and the percentage of each color on the picture.
+
+## Image correction sections
+
 Since the color chart is a key part of the analysis, we have included a section to manipulate the image to create a custom color chart.
-* `rotation of the color chart`:  You can rotate the image 180 degrees to the right or to the left
-* `dewarp the image`:  You can fix the perspective of the image. This is useful when the image is not taken from the front and the OCR can not detect the labels
-* `manual selection`: You can select the manually the colors one by one from the chart segments from the image (The last resort if the OCR can not detect the labels , is time comsuming but it works)
+- Rotation of the color chart: You can rotate the image 180 degrees to the right or the left
+- Dewarp the image: You can fix the image's perspective. This is useful when the OCR cannot detect the labels
+- Manual selection: You can select the colors one by one from the chart segments from the image (in case the OCR cannot detect the labels)
 
-## Several images at the same time - Batch processing
-If you have several images to analyze you can use the batch processing section.
-* Load the color chart or select the custom color chart from a reprensentative image on the firts section
-* Go to the batch processing section
-* Select the images you want to analyze
-* Press the button to start the batch processing
-* Wait for the results
+## Batch mode
 
-The results will be saved on a zip file that you can download and check the results. This process will take more time since it will process all coral detected on all the images you have selected.
+This option allows the analysis of several images simultaneously using the same color chart.
 
+## Multi-segment analysis
 
-
-## Badges
-
-Add badges from somewhere like: [shields.io](https://shields.io/)
-
-[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
-
-## Screenshots
-
+This option allows the analysis of several segments at the same time.
 """
 )
-# st.image("images/ColorChart.jpg", caption="Screenshot of the color chart", use_column_width=True)
-# st.image("images/Figure_for_neus_paper-underwater-app.png", caption="Conceptual workflow", use_column_width=True)
+st.image("app/images/Coral-Cat_app.png", caption="App usage", use_column_width=True)
